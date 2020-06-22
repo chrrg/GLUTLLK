0a12ae306807677dc74cd1ed49d77c35b071b95a
CH <caohongchrrg@qq.com>
2020-06-22 16:00:33 +0800

    upload

--- app/src/main/java/cn/edu/glut/llk/CHCanvasGame.java
+++ app/src/main/java/cn/edu/glut/llk/CHCanvasGame.java
@@ -1,17 +1,66 @@
 package cn.edu.glut.llk;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.ImageView;
-class GameObject{
-    public void setPic(){
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+
+interface GameObjectDraw{
+    void onDraw(Canvas c,Paint paint);
+}
+interface GameInit{
+    void onInit();
+}
+class GameObject{
+    int x,y,w,h;
+    private int backColor=0;
+    private GameObjectDraw od=null;
+    private Bitmap baseBitmap=null;
+    private int pic=0;
+    public void setPic(int pic){
+        this.pic=pic;
+    }
+    public void clear(){
+        baseBitmap=null;
+    }
+    void setBackColor(int backColor) {
+        this.backColor=backColor;
+    }
+    void draw(Canvas c,Paint paint){
+        if(baseBitmap==null) {
+            baseBitmap = Bitmap.createBitmap(this.w, this.h, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(baseBitmap);
+            if(backColor!=0){
+                canvas.drawColor(backColor);
+            }else if (this.od != null){
+                this.od.onDraw(canvas, paint);
+            }else return;
+        }
+        c.drawBitmap(baseBitmap,this.x,this.y,paint);
+    }
+    void set(int x, int y, int w, int h) {
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+    }
+    void setDraw(GameObjectDraw od){
+        this.od=od;
     }
 }
+
 class GameCamera{
     private int x;
     private int y;
@@ -25,29 +74,121 @@ class GameCamera{
         this.y=y;
     }
 }
+class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    public GameSurfaceView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
 class CHCanvasGame {
+    private Vector<GameObject> obj=new Vector<GameObject>();
     private GameCamera camera;
+    private Bitmap baseBitmap;
+    private Canvas canvas;
+    private Paint paint;
+    SurfaceView surfaceview;
+    SurfaceHolder surfaceholder;
+    private int w,h;
     CHCanvasGame(){
 
+    }
+    int getWidth(){
+        return this.w;
+    }
+    int getHeight(){
+        return this.h;
     }
     void setCamera(GameCamera c){
         this.camera=c;
     }
-    void init(Activity self, int id){
-        final Paint paint = new Paint();
+    void addGameObject(GameObject o){
+        obj.add(o);
+    }
+    private void drawOnce(Canvas c,Paint paint){
+        for(GameObject ob:obj){
+            ob.draw(c,paint);
+        }
+    }
+    void init(Activity activity, int id, final GameInit init){
+        paint = new Paint();
         paint.setStrokeWidth(5);
         paint.setColor(Color.RED);
-        final ImageView image = self.findViewById(id);
-        image.post(new Runnable() {
+        surfaceview = activity.findViewById(id);
+        surfaceholder = surfaceview.getHolder();
+        surfaceholder.addCallback(new SurfaceHolder.Callback(){
             @Override
-            public void run() {
-                RectF r = new RectF(200, 200, 400, 500);
-                Bitmap baseBitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.ARGB_8888);
-                Canvas canvas = new Canvas(baseBitmap);
-                canvas.drawRect(r, paint);
-                image.setImageBitmap(baseBitmap);
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            canvas = surfaceholder.lockCanvas();
+                            if(canvas!=null) {
+                                drawOnce(canvas, paint);
+                                surfaceholder.unlockCanvasAndPost(canvas);
+                            }
+                            try {
+                                Thread.sleep(1); // 这个就相当于帧频了，数值越小画面就越流畅
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
             }
         });
+//        canvas = surfaceholder.lockCanvas();
+//        Log.e("www", String.valueOf(surfaceview.getWidth()));
+//        return;
+        surfaceview.post(new Runnable() {
+            @Override
+            public void run() {
+                w=surfaceview.getWidth();
+                h=surfaceview.getHeight();
+//                baseBitmap = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888);
+//                canvas = new Canvas(baseBitmap);
+                init.onInit();
+            }
+        });
+//        new Timer().schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//
+//                if(canvas==null||baseBitmap==null)return;
+//                drawOnce(canvas,paint);
+//                image.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        image.setImageBitmap(baseBitmap);
+//                    }
+//                });
+//            }
+//        },0,10);
+
     }
 
 }
--- app/src/main/java/cn/edu/glut/llk/MainActivity.java
+++ app/src/main/java/cn/edu/glut/llk/MainActivity.java
@@ -2,7 +2,9 @@ package cn.edu.glut.llk;
 
 
 import android.app.Activity;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +12,12 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends Activity {
+    CHCanvasGame game=null;
+    GameCamera camera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +25,39 @@ public class MainActivity extends Activity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){Window window = getWindow();window.setStatusBarColor(Color.WHITE);}
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_main);
-        CHCanvasGame game = new CHCanvasGame();//初始化游戏引擎
-        game.init(this, R.id.canvas);//设置参数
-        GameCamera camera = new GameCamera();
-        game.setCamera(camera);//设置2d摄像机
+        if(game==null) {
+            game = new CHCanvasGame();//初始化游戏引擎
+            camera = new GameCamera();
+            game.setCamera(camera);//设置2d摄像机
+            game.init(this, R.id.canvas, new GameInit() {
+                @Override
+                public void onInit() {
+                    GameObject backGround = new GameObject();//新建一个背景
+                    backGround.set(0, 0, game.getWidth(), game.getHeight());
+                    backGround.setBackColor(Color.WHITE);
+                    game.addGameObject(backGround);
+
+                    final GameObject box = new GameObject();//新建一个游戏内对象
+                    box.set(50, 50, 100, 100);//设置对象X
+                    box.setDraw(new GameObjectDraw() {
+                        @Override
+                        public void onDraw(Canvas c, Paint paint) {
+                            c.drawCircle(20, 20, 15, paint);
+                        }
+                    });
+                    game.addGameObject(box);
+
+                    GameObject box2 = new GameObject();//新建一个游戏内对象
+                    box2.set(110, 100, 150, 100);//设置对象X
+                    game.addGameObject(box2);
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            box.y += 1;
+                        }
+                    }, 0, 10);
+                }
+            });//设置参数
+        }
     }
 }

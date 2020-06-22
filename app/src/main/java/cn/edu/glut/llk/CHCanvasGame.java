aeaf12a86ab64790b7905f80493c57c1093c634d
CH <caohongchrrg@qq.com>
2020-06-22 11:39:08 +0800

    upload

--- app/src/main/java/cn/edu/glut/llk/CHCanvasGame.java
+++ app/src/main/java/cn/edu/glut/llk/CHCanvasGame.java
@@ -1,16 +1,31 @@
 package cn.edu.glut.llk;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.widget.ImageView;
+
 public class CHCanvasGame {
 //    private static CHCanvasGame self=null;
 //    public static CHCanvasGame getInstance(){//得到单一实例
 //        if(self==null)self=new CHCanvasGame();
 //        return self;
 //    }
-    public CHCanvasGame(){
+    CHCanvasGame(){
 
     }
-    public void init(){
-
+    void init(Activity self, int id){
+        Paint paint = new Paint();
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.RED);
+        ImageView image = self.findViewById(id);
+        RectF r = new RectF(200, 200, 400, 500);
+        Bitmap baseBitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(baseBitmap);
+        canvas.drawRect(r, paint);
     }
 
 }
--- app/src/main/java/cn/edu/glut/llk/MainActivity.java
+++ app/src/main/java/cn/edu/glut/llk/MainActivity.java
@@ -9,18 +9,15 @@ import android.view.View;
 import android.view.Window;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.setStatusBarColor(Color.WHITE);
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){Window window = getWindow();window.setStatusBarColor(Color.WHITE);}
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_main);
+        ///////////////////////////////////////////////////////
+        CHCanvasGame game = new CHCanvasGame();//初始化游戏引擎
+        game.init(this, R.id.canvas);//设置参数
     }
 }

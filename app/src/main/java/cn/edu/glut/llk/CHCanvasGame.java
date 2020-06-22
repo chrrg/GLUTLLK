94cdb46b57ec33b7a6f77a9279b2123fc34489f4
CH <caohongchrrg@qq.com>
2020-06-22 11:23:04 +0800

    upload

---
+++ app/src/main/java/cn/edu/glut/llk/CHCanvasGame.java
@@ -0,0 +1,16 @@
+package cn.edu.glut.llk;
+
+public class CHCanvasGame {
+//    private static CHCanvasGame self=null;
+//    public static CHCanvasGame getInstance(){//得到单一实例
+//        if(self==null)self=new CHCanvasGame();
+//        return self;
+//    }
+    public CHCanvasGame(){
+
+    }
+    public void init(){
+
+    }
+
+}

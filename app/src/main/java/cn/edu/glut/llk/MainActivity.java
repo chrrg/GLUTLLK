e6abe484b3eb99b96292e02988b2f8d1e2fb64ac
CH <caohongchrrg@qq.com>
2020-06-22 10:43:28 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/glut/llk/MainActivity.java
@@ -0,0 +1,26 @@
+package cn.edu.glut.llk;
+
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setStatusBarColor(Color.WHITE);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+
+        setContentView(R.layout.activity_main);
+    }
+}
